4b3f5c46151be727dca91bb1c67e40844f537fb5
yhwba <59465345+yhwba@users.noreply.github.com>
2020-05-25 12:07:03 +0900

    3strike 처리는 1 초 후로 변경 - 원래는 곧바로 였음

--- app/src/main/java/kr/co/yhw/baseballgame_20200525/MainActivity.java
+++ app/src/main/java/kr/co/yhw/baseballgame_20200525/MainActivity.java
@@ -182,17 +182,24 @@ public class MainActivity extends BaseActivity {
 
 //        3S 라면 축하메세지 + 몇번만에 맞췄는지 + 입력 불가하도록 막아주기
         if (strikeCount == 3) {
-            messages.add(new Message("정답입니다", "Cpu"));
-            messages.add(new Message(String.format("%d회만에 맞췄습니다", tryCount), "Cpu"));
-            messageAdapter.notifyDataSetChanged();
-            binding.messageListView.smoothScrollToPosition(messages.size() - 1);
+
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    messages.add(new Message("정답입니다", "Cpu"));
+                    messages.add(new Message(String.format("%d회만에 맞췄습니다", tryCount), "Cpu"));
+                    messageAdapter.notifyDataSetChanged();
+                    binding.messageListView.smoothScrollToPosition(messages.size() - 1);
 
 //            EditText와 button을 더이상 사용하지 못하도록 막아주는 코드
-            binding.numEdt.setEnabled(false);
-            binding.sendBtn.setEnabled(false);
+                    binding.numEdt.setEnabled(false);
+                    binding.sendBtn.setEnabled(false);
 
 //            종료 메세지
-            Toast.makeText(mContext, "이용해주셔서 감사합니다아~", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(mContext, "이용해주셔서 감사합니다아~", Toast.LENGTH_SHORT).show();
+                }
+            },1000);
+
         }
 
 
