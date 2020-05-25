377c5ee75611d07738e70825be32c24d1c615850
yhwba <59465345+yhwba@users.noreply.github.com>
2020-05-25 13:18:45 +0900

    SplashActivity 추가 이 화면을 시작점으로 설정

---
+++ app/src/main/java/kr/co/yhw/baseballgame_20200525/SplashActivity.java
@@ -0,0 +1,26 @@
+package kr.co.yhw.baseballgame_20200525;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        setupEvents();
+        setVaules();
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setVaules() {
+
+    }
+}
